a9ebfd135e545266148f33f3c903f848e64c2e23
Karita Kamula <2306054@edu.karelia.fi>
2024-11-06 20:42:18 +0200

    Initial commit with Tehtava4 solution and projects

--- AppLogger/obj/Debug/net8.0/AppLogger.AssemblyInfo.cs
+++ AppLogger/obj/Debug/net8.0/AppLogger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872c5cf019a3438bf31fa21234fecc4b41838aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
